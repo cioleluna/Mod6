d9719e336b8d03e8b9667e177fbb588b069df7c9
cioleluna <preciosa.211006@gmail.com>
2026-04-06 01:03:05 +0700

    commit message

--- Program.cs
+++ Program.cs
@@ -1,21 +1,41 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Linq.Expressions;
 class SayaMusicTrack
 {
     private int id;
     private string title;
-    private string playCount;
+    private int playCount;
 
     public SayaMusicTrack(string title)
     {
+        Debug.Assert(title != null, "Judul tidak boleh kosong");
+        Debug.Assert(title.Length <= 100, "Judul maksimal 100 karakter");
+
         Random random = new Random();
         id = random.Next(10000, 100000);
+        
         this.title = title;
-        playCount = "0";
+        playCount = 0;
     }
 
     public void IncreasePlayCount(int jumlah)
     {
-        playCount += jumlah;
+        Debug.Assert(jumlah <= 10000000, "Penambahan play count maksimal 10.000.000");
+
+        try
+        {
+            checked 
+            {
+                playCount += jumlah;
+            }
+        }
+        catch (OverflowException)
+        {
+            Console.WriteLine("Error: Play count melebihi batas maksimum integer!");
+            throw;
+        }
+
     }
     public void PrintTrackDetails()
     {
@@ -30,18 +50,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        SayaMusicTrack lagu1 = new SayaMusicTrack("18 - One Direction");
-        SayaMusicTrack lagu2 = new SayaMusicTrack("I Would - One Direction");
-        SayaMusicTrack lagu3 = new SayaMusicTrack("Heart Attack - One Direction");
+        try
+        {
 
-        lagu1.IncreasePlayCount(5);
-        lagu2.IncreasePlayCount(8);
-        lagu3.IncreasePlayCount(10);
 
-        lagu1.PrintTrackDetails();
-        lagu2.PrintTrackDetails();
-        lagu3.PrintTrackDetails();
+            SayaMusicTrack lagu1 = new SayaMusicTrack("18 - One Direction");
+            SayaMusicTrack lagu2 = new SayaMusicTrack("I Would - One Direction");
+            SayaMusicTrack lagu3 = new SayaMusicTrack("Heart Attack - One Direction");
 
+            lagu1.IncreasePlayCount(5000000);
+            lagu2.IncreasePlayCount(8000000);
+            lagu3.IncreasePlayCount(10);
+
+            lagu1.PrintTrackDetails();
+            lagu2.PrintTrackDetails();
+            lagu3.PrintTrackDetails();
+
+            for (int i = 0; i < 1000; i++)
+            {
+                lagu2.IncreasePlayCount(10000000);
+            }
+
+            lagu2.PrintTrackDetails();
+        }
+        
+        catch (Exception e)
+         {
+            Console.WriteLine("Terjadi error: " + e.Message);
+        }
         Console.ReadLine();
     }
 }
--- obj/Debug/net10.0/TP_MODUL6_103022400026.AssemblyInfo.cs
+++ obj/Debug/net10.0/TP_MODUL6_103022400026.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TP_MODUL6_103022400026")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+761edb53104b1061639b86766d608580194091a7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2fe210a7674a5ce11655e4ce69a2badd496d59d")]
 [assembly: System.Reflection.AssemblyProductAttribute("TP_MODUL6_103022400026")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TP_MODUL6_103022400026")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
